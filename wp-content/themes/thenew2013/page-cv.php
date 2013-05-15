65315c71d40be7bc10da22954c638d49152509ba
thenew <remy@thenew.fr>
2013-05-16 00:18:58 +0200

    begin page CV

--- wp-content/themes/thenew2013/page-cv.php
+++ wp-content/themes/thenew2013/page-cv.php
@@ -5,7 +5,7 @@ get_header(); the_post();
 <nav id="sidenav" class="sidenav">
     <?php include TEMPLATEPATH .'/nav.php'; ?>
 </nav>
-<div id="main-col" class="main-col">
+<div id="main-col" class="main-col template-cv">
     <h1 class="post-title"><?php the_title(); ?></h1>
     <div class="blabla post-content">
         <?php the_content(); ?>
